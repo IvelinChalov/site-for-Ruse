0afe468038dda86c8f7bef04bdd795d1266c9adf
Ivelin Chalov <ivelin_chalov@abv.bg>
2016-02-20 20:38:49 +0200

    First commit

---
+++ javas1.js
@@ -0,0 +1,48 @@
+function galeriazab ()
+{
+var kar = new Array("zabelezitelnosti/alio6as.jpg","zabelezitelnosti/babas.jpg","zabelezitelnosti/casovnikas.jpg","zabelezitelnosti/dohods.jpg","zabelezitelnosti/istoriqs.jpg","zabelezitelnosti/panteons.jpg","zabelezitelnosti/statyqs.jpg","zabelezitelnosti/transports.jpg","zabelezitelnosti/vazas.jpg");
+    kar1 = new Array("zabelezitelnosti/alio6a.jpg","zabelezitelnosti/baba.jpg","zabelezitelnosti/casovnika.jpg","zabelezitelnosti/dohod.jpg","zabelezitelnosti/istoriq.jpg","zabelezitelnosti/panteon.jpg","zabelezitelnosti/statyq.jpg","zabelezitelnosti/transport.jpg","zabelezitelnosti/vaza.jpg");
+	idta= new Array("img1","img2","img3","img4","img5","img6","img7","img8","img9"); 
+	idta1= new Array("img01","img02","img03","img04","img05","img06","img07","img08","img09");
+	ad=new Array("a1","a2","a3","a4","a5","a6","a7","a8","a9");
+  i=0;
+  for (i=0;i<=8;i++)
+  {
+  document.images[idta[i]].src =kar[i];
+  document.getElementById(ad[i]).href=kar1[i];
+  document.images[idta1[i]].src =kar[i];
+  }
+}
+
+function galeriaruse ()
+{
+var kar = new Array("izryse/dynav1s.jpg","izryse/dynav2s.jpg","izryse/dynav3s.jpg","izryse/izruses.jpg","izryse/malls.jpg","izryse/otmolas.jpg","izryse/parks.jpg","izryse/plo6tads.jpg","izryse/unizads.jpg");
+    kar1 = new Array("izryse/dynav1.jpg","izryse/dynav2.jpg","izryse/dynav3.jpg","izryse/izruse.jpg","izryse/mall.jpg","izryse/otmola.jpg","izryse/park.jpg","izryse/plo6tad.jpg","izryse/unizad.jpg");
+	idta= new Array("img1","img2","img3","img4","img5","img6","img7","img8","img9"); 
+	idta1= new Array("img01","img02","img03","img04","img05","img06","img07","img08","img09");
+	ad=new Array("a1","a2","a3","a4","a5","a6","a7","a8","a9");
+  i=0;
+   for (i=0;i<=8;i++)
+  {
+  document.images[idta[i]].src =kar[i];
+  document.getElementById(ad[i]).href=kar1[i];
+  document.images[idta1[i]].src =kar[i];
+  }
+}
+
+function galeriay4i()
+{
+  var  kar= new Array("y4ili6ta/aeg1s.jpg","y4ili6ta/aeg2s.jpg","y4ili6ta/aeg3s.jpg","y4ili6ta/pgd1s.jpg","y4ili6ta/pgd2s.jpg","y4ili6ta/pgd3s.jpg","y4ili6ta/uni1s.jpg","y4ili6ta/uni2s.jpg","y4ili6ta/uni3s.jpg");
+    kar1= new Array("y4ili6ta/aeg1.jpg","y4ili6ta/aeg2.jpg","y4ili6ta/aeg3.jpg","y4ili6ta/pgd1.jpg","y4ili6ta/pgd2.jpg","y4ili6ta/pgd3.jpg","y4ili6ta/uni1.jpg","y4ili6ta/uni2.jpg","y4ili6ta/uni3.jpg");
+	idta= new Array("img1","img2","img3","img4","img5","img6","img7","img8","img9"); 
+	idta1= new Array("img01","img02","img03","img04","img05","img06","img07","img08","img09");
+	ad=new Array("a1","a2","a3","a4","a5","a6","a7","a8","a9");
+    
+	 i=0;
+   for (i=0;i<=8;i++)
+  {
+  document.images[idta[i]].src =kar[i];
+  document.getElementById(ad[i]).href=kar1[i];
+  document.images[idta1[i]].src =kar[i];
+  }
+}
